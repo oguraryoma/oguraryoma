43d103991b1702e23d5624e2ac9b97566793577d
nagiu <nagiu@LAPTOP-J027RQTT>
2025-09-27 20:30:37 +0900

    メソッド（上級）の提出

--- HelloWorld_oguraryoma/src/curriculum_New_question/Curriculum_New_1_18.java
+++ HelloWorld_oguraryoma/src/curriculum_New_question/Curriculum_New_1_18.java
@@ -1,25 +1,73 @@
 package curriculum_New_question;
 
-public class Curriculum_New_1_18{
-	// Q1：引数に文字列型と整数型をいれてコンソールに「Hello JavaSE 11」と出力するメソッドを作成してください。
-	
-	// Q2：引数に整数を渡すと渡した値同士を乗算しコンソールに出力するメソッドを作成してください
-		
-	// Q3：引数として整数の配列を渡すと、受け取った値を順番にコンソールに出力するメソッドを作成してください
-			
-	// Q4：Q2をオーバーロードして引数を小数2つに変更し、引数同士を和算しコンソールに出力してください。
-		
-	// Q5：引数に整数を渡すと、1～100までのランダムな数字を引数の回数分格納して
-	// 格納した値を順番にコンソールで出力後、格納した値を返すメソッドを作成してください。
-	// ※0は出力＆格納しないようにしてください。
+import java.util.Random;
 
-	// Q6：引数にQ5で作成したメソッドの返り値を受け取り、受け取った配列の要素の平均値をコンソールに出力するメソッドを作成してください。
-	// ※小数点以下も表示されるようにしてください。
-	
-	// Q7：引数にQ6で作成したメソッドの返り値を受け取り、受け取った値が50以上ならばtrueそれ以外はfalseを返しコンソールに出力してください
+public class Curriculum_New_1_18 {
+	// Q1
+	public static void greet(String text, int version) {
+		System.out.println("Hello " + text + " " + version);
+	}
+	// Q2
+	public static void multiply(int a, int b) {
+		System.out.println(a * b);
+	}
+	// Q3
+	public static void printArray(int[] numbers) {
+		for (int num : numbers) {
+			System.out.println(num);
+		}
+	}
+	// Q4
+	public static void multiply(double x, double y) {
+		System.out.println(x + y);
+	}
+	// Q5
+	public static int[] generateNumbers(int count) {
+		Random rand = new Random();
+		int[] numbers = new int[count];
+
+		for (int i = 0; i < count; i++) {
+			numbers[i] = rand.nextInt(100) + 1; // 1～100
+			System.out.println(numbers[i]);
+		}
+
+		return numbers;
+	}
+	// Q6: 平均を計算して返すメソッド
+	public static double calculateAverage(int[] nums) {
+		double sum = 0;
+
+		for (int i = 0; i < nums.length; i++) {
+			sum += nums[i];
+		}
+
+		double average = sum / nums.length;
+		System.out.println("平均値: " + average);
+		return average;
+	}
+	// Q7
+	public static boolean isAverageOver50(double average) {
+		boolean result = average >= 50;
+		System.out.println("50以上か？: " + result);
+		return result;
+	}
 
 	public static void main(String[] args) {
-    // 作成したメソッドをここで呼び出してください
-	
+		// Q1
+		greet("JavaSE", 11);
+		// Q2
+		multiply(64, 5);
+		// Q3
+		int[] data = { 10, 20, 30, 40, 50 };
+		printArray(data);
+		// Q4
+		multiply(3.5, 14.5);
+		// Q5
+		int[] result = generateNumbers(5);
+		System.out.println("戻り値の最初の要素: " + result[0]);
+		// Q6
+		double avg = calculateAverage(result);
+		// Q7
+		boolean check = isAverageOver50(avg);
 	}
 }
