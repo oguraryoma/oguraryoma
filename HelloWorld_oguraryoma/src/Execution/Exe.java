46c541b7690c460297202608a185725a07b7699a
nagiu <nagiu@LAPTOP-J027RQTT>
2025-10-02 22:43:13 +0900

    パッケージをコンパイルの提出

---
+++ HelloWorld_oguraryoma/src/Execution/Exe.java
@@ -0,0 +1,12 @@
+package Execution;
+
+import JavaProject.Theme2;
+
+public class Exe {
+	public static void main(String[] args) {
+		Theme2 theme = new Theme2();
+		theme.greet();
+		theme.showDay();
+		
+	}	
+}
--- HelloWorld_oguraryoma/src/basicClass/Dog.java
+++ HelloWorld_oguraryoma/src/basicClass/Dog.java
@@ -2,7 +2,7 @@ package basicClass;
 
 public class Dog {
 	// Q1：フィールドに動物の名前の変数を定義してください。
-	String name;
+	 String name;
 
 	// Q2：フィールドに動物の数の変数を定義してください。
 	int count;
