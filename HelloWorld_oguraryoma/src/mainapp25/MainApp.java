b96c96d694e4363fbee2b297baaefa6ad1d33868
nagiu <nagiu@LAPTOP-J027RQTT>
2025-10-21 23:02:17 +0900

    継承の基本の課題(実行用）を送ります。

---
+++ HelloWorld_oguraryoma/src/mainapp25/MainApp.java
@@ -0,0 +1,22 @@
+package mainapp25;
+
+import java.util.Scanner;
+import curriculum_25.Hero; // ← パッケージ名を修正
+
+public class MainApp {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("名前を入力してください：");
+        String name = scanner.nextLine();
+
+        Hero hero = new Hero(name);
+
+        System.out.println();
+        System.out.println("こんにちは「" + hero.getName() + "」さん！");
+        hero.showStatus();
+        System.out.println("さあ冒険に出かけよう！");
+
+        scanner.close();
+    }
+}
